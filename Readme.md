649ca5070842a8df1dbed3328618b6df8be4fcb9
Aleksandar Angeloski <aleksandar.angeloski@deliveryhero.com>
2016-01-25 09:59:44 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# A basic setup for front-end project
+* Set up in a TDD fashion
+* Stack: Gulp, npm, Browserify, Karma, Mocha and Chai
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// Gulp Dependencies
+var gulp = require('gulp');
+var karmaServer = require('karma').Server;
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+
+// Run test once and exit
+gulp.task('test', function (done) {
+  new karmaServer({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+
+// Watch for file changes and re-run tests on each change
+gulp.task('tdd', function (done) {
+  new karmaServer({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start();
+});
+
+
+// Bundle js files
+gulp.task('bundle', function() {
+    return browserify('./src/utils/utils.js').bundle()
+        // vinyl-source-stream makes the bundle compatible with gulp
+        .pipe(source('bundle.js')) // Desired filename
+        // Output the file
+        .pipe(gulp.dest('./dist/'));
+});
+
+
+gulp.task('default', ['tdd']);
